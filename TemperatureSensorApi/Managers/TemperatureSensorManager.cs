a9352cf5d1f82d995af6eb2fe28e46c57d9d9d4d
AntoineThach <Antoine.t@olympp.fr>
2023-09-17 16:30:15 +0200

    feature/temperature-status : Adding GetTemperature mood

--- TemperatureSensorApi/Managers/TemperatureSensorManager.cs
+++ TemperatureSensorApi/Managers/TemperatureSensorManager.cs
@@ -8,10 +8,30 @@ namespace TemperatureSensorApi.Managers
     public class TemperatureSensorManager : ITemperatureSensorManager
     {
         private readonly ITemperatureSensorRepository _temperatureSensorRepository;
+        private readonly ITemperatureStatusManager _temperatureStatusManager;
 
-        public TemperatureSensorManager(ITemperatureSensorRepository temperatureSensorRepository)
+        public TemperatureSensorManager(ITemperatureSensorRepository temperatureSensorRepository, ITemperatureStatusManager temperatureStatusManager)
         {
             _temperatureSensorRepository = temperatureSensorRepository;
+            _temperatureStatusManager = temperatureStatusManager;
+        }
+
+        public async Task<string> GetTemperatureMood()
+        {
+            var mood = "NORMAL";
+            var currentTemperature = await GetTemperature();
+            var coldTemperature = await _temperatureStatusManager.GetColdTemperature();
+            var hotTemperature = await _temperatureStatusManager.GetHotTemperature();
+            var warmLowTemperatureLimit = await _temperatureStatusManager.GetWarmTemperatureLimit(true);
+            var warmHighTemperatureLimit = await _temperatureStatusManager.GetWarmTemperatureLimit(false);
+
+            if (currentTemperature >= hotTemperature)
+                mood = "HOT";
+            else if (currentTemperature < coldTemperature)
+                mood = "COLD";
+            else if (currentTemperature >= warmLowTemperatureLimit && currentTemperature < warmHighTemperatureLimit)
+                mood = "WARM";
+            return mood;
         }
 
         public async Task UpdateTemperature(string value)
